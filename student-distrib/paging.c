164cc39e821f19ff303bc80ada6f7764fa3ae2ef
Papanikolaou <vasilis2@illinois.edu>
2022-10-16 20:48:38 -0500

    fixed typo in lib.c, added pagetable entry for video memory in setup

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -34,9 +34,16 @@ void spawnDir()
 	int i;
 	for(i = 0;i < 1024;i++)
 	{
-		pageDir[i].val = 0x2;	//enable rw on everything
-	}				//(since p isn't enabled nothing is accessible)
+		pageDir[i].val = 0;	//initialize table with nothing accessible
+	}
 	return;
+}
+
+void spawnTbl(union tblEntry tab[1024])
+{
+	int i;
+	for(i = 0;i < 1024;i++)
+		tab[i].val = 0;
 }
 
 /*
@@ -49,13 +56,19 @@ void setup()
 {
 	union dirEntry vidTable;
 	union dirEntry kernel;	//this is the 4MiB page for the kernel
+	union tblEntry vidPg;
 	vidTable.val = (unsigned)table | 3;	//trick from OSdev, sets p and rw
 	kernel.val = 0;
 	kernel.whole.p = 1;
 	kernel.whole.rw = 1;
 	kernel.whole.ps = 1;
-	kernel.whole.add_22_31 = 0x400000 >> 22;	//0x400000 is 4mb kernel.val |= 0x4000000 also works
+	kernel.whole.add_22_31 = 0x400000 >> 22;	//0x400000 is 4mb kernel.val |= 0x400000 also works
+	vidPg.val = 0x3;	//sets p and rw bits
+	vidPg.ent.add = 0xB8000 >> 12;	//bits 31-12 (just 0xB8)
+	spawnTbl(table);
+	table[0xB8] = vidPg;	//vidmem starts at 0xB8000, divide by 4KiB to get index
 	spawnDir();
+	pageDir[0] = vidTable;
 	pageDir[1] = kernel;
 	pageEnable();
 	return;
