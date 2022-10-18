5cfa57696df4d7839f027d76c629e93b17160e9f
Amartya Bhattacharya <amartyacharya@gmail.com>
2022-10-18 00:41:12 -0500

    added keyboard shift support

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -7,130 +7,95 @@
 #include "lib.h"
 #include "types.h"
 
+
 /* Local variables */
-char scancodes[59] = {
-    '\0',                                       // 0x00
-    '\0',                                       // 0x01
-    '1',                                        // 0x02
-    '2',                                        // 0x03
-    '3',                                        // 0x04
-    '4',                                        // 0x05
-    '5',                                        // 0x06
-    '6',                                        // 0x07
-    '7',                                        // 0x08
-    '8',                                        // 0x09
-    '9',                                        // 0x0A
-    '0',                                        // 0x0B
-    '-',                                        // 0x0C
-    '=',                                        // 0x0D
-    '\b',                                       // 0x0E
-    '\t',                                       // 0x0F
-    'q',                                        // 0x10
-    'w',                                        // 0x11
-    'e',                                        // 0x12
-    'r',                                        // 0x13
-    't',                                        // 0x14
-    'y',                                        // 0x15
-    'u',                                        // 0x16
-    'i',                                        // 0x17
-    'o',                                        // 0x18
-    'p',                                        // 0x19
-    '[',                                        // 0x1A
-    ']',                                        // 0x1B
-    '\r',                                       // 0x1C
-    '\0',                                       // 0x1D
-    'a',                                        // 0x1E
-    's',                                        // 0x1F
-    'd',                                        // 0x20
-    'f',                                        // 0x21
-    'g',                                        // 0x22
-    'h',                                        // 0x23
-    'j',                                        // 0x24
-    'k',                                        // 0x25
-    'l',                                        // 0x26
-    ';',                                        // 0x27
-    '\'',                                       // 0x28
-    '`',                                        // 0x29
-    '\0',                                       // 0x2A // shift
-    '\\',                                       // 0x2B
-    'z',                                        // 0x2C
-    'x',                                        // 0x2D
-    'c',                                        // 0x2E
-    'v',                                        // 0x2F
-    'b',                                        // 0x30
-    'n',                                        // 0x31
-    'm',                                        // 0x32
-    ',',                                        // 0x33
-    '.',                                        // 0x34
-    '/',                                        // 0x35
-    '\0',                                       // 0x36 // shift
-    '\0',                                       // 0x37
-    '\0',                                       // 0x38
-    ' ',                                        // 0x39
-    '\0',                                       // 0x3A // caps lock
+uint8_t key_status = 0;
+// +-------+------+--------+-----+-----+------+------+-------+
+// |   7   |   6  |   5    |  6  |  3  |  2   |  1   |   0   |
+// +-------+------+--------+-----+-----+------+------+-------+
+// |   reserved   | scroll | num | alt | ctrl | caps | shift |
+// +----------+---+--------+-----+-----+------+------+-------+
+
+
+unsigned char keymap[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 0x09 */
+    '9', '0', '-', '=', '\b',	/* Backspace */
+    '\t',			/* Tab */
+    'q', 'w', 'e', 'r',	/* 0x10 - 0x13 */
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* Ctrl */  
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 0x1E - 0x27 */
+    '\'', '`',   0,		/* Shift */ 
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 0x2B - 0x31 */
+    'm', ',', '.', '/',   0,				/* Shift */
+    '*',
+    0,	/* Alt */
+    ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 0x3B - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 - 0x44 */
+    0,	/* 0x45 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+    '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+    '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
 };
 
 
-char scancodes_upper[59] = {
-    '\0',                                       // 0x00
-    '\0',                                       // 0x01
-    '!',                                        // 0x02
-    '@',                                        // 0x03
-    '#',                                        // 0x04
-    '$',                                        // 0x05
-    '%',                                        // 0x06
-    '^',                                        // 0x07
-    '&',                                        // 0x08
-    '*',                                        // 0x09
-    '(',                                        // 0x0A
-    ')',                                        // 0x0B
-    '_',                                        // 0x0C
-    '+',                                        // 0x0D
-    '\b',                                       // 0x0E
-    '\t',                                       // 0x0F
-    'Q',                                        // 0x10
-    'W',                                        // 0x11
-    'E',                                        // 0x12
-    'R',                                        // 0x13
-    'T',                                        // 0x14
-    'Y',                                        // 0x15
-    'U',                                        // 0x16
-    'I',                                        // 0x17
-    'O',                                        // 0x18
-    'P',                                        // 0x19
-    '{',                                        // 0x1A
-    '}',                                        // 0x1B
-    '\r',                                       // 0x1C
-    '\0',                                       // 0x1D
-    'A',                                        // 0x1E
-    'S',                                        // 0x1F
-    'D',                                        // 0x20
-    'F',                                        // 0x21
-    'G',                                        // 0x22
-    'H',                                        // 0x23
-    'J',                                        // 0x24
-    'K',                                        // 0x25
-    'L',                                        // 0x26
-    ':',                                        // 0x27
-    '"',                                        // 0x28
-    '~',                                        // 0x29
-    '\0',                                       // 0x2A
-    '|',                                        // 0x2B
-    'Z',                                        // 0x2C
-    'X',                                        // 0x2D
-    'C',                                        // 0x2E
-    'V',                                        // 0x2F
-    'B',                                        // 0x30
-    'N',                                        // 0x31
-    'M',                                        // 0x32
-    '<',                                        // 0x33
-    '>',                                        // 0x34
-    '?',                                        // 0x35
-    '\0',                                       // 0x36
-    '\0',                                       // 0x37
-    '\0',                                       // 0x38
-    ' ',                                        // 0x39
-    '\0',                                       // 0x3A // caps lock
+unsigned char keymap_upper[128] =
+{
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	/* 0x09 */
+    '(', ')', '_', '+', '\b',	/* Backspace */
+    '\t',			/* Tab */
+    'Q', 'W', 'E', 'R',	/* 0x13 */
+    'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',	/* Enter key */
+    0,			/* Ctrl */  
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',	/* 0x27 */
+    '"', '~',   0,		/* Shift */ 
+    '|', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 0x31 */
+    'M', '<', '>', '?',   0,				/* Shift */
+    '*',
+    0,	/* Alt */
+    ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 0x3B - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 - 0x44 */
+    0,	/* 0x45 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+    '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+    '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
 };
 
 
@@ -148,7 +113,7 @@ void keyboard_init(void) {
 
 /*
  * keyboard_handler
- * Converts scancode to char and prints to screen
+ * Handles the keyboard interrupt, converts scancode to char and prints to screen
  * DESCRIPTION: Handles keyboard interrupts
  * INPUTS: none
  * OUTPUTS: none
@@ -156,28 +121,82 @@ void keyboard_init(void) {
  * SIDE EFFECTS: Prints the character corresponding to the key pressed
  */
 void keyboard_handler(void) {
-    uint8_t scancode = inb(KEYBOARD_DATA_PORT);     /* get scancode */
-    // check if scancode is valid
-    // if (scancode > 0x3A) {
-    //     // invalid scancode
-    //     return;
-    // }
-    // // check if scancode is a special key
-    // if (scancode == 0x2A || scancode == 0x36) {
-    //     // shift key
-    //     // handle
-    //     return;
-    // }
-    // if (scancode == 0x3A) {
-    //     // caps lock key
-    //     // handle
-    //     return;
-    // }
-    // valid scancode
-    // for shift just add the offset for the array
-    if (scancode <= sizeof(scancodes)) {    /* check if scancode is valid */
-        char c = scancodes[scancode];       /* get char from scancode */
-        putc(c);                            /* print char */
+    uint8_t scancode = inb(KEYBOARD_DATA_PORT);     /* read scancde from keyboard data port */
+    // update key_status
+    
+    if (scancode & 0x80) {
+        // key released
+        // handle key up event, check that shift, ctrl, alt, caps lock, num lock, scroll lock are not pressed anymore
+        // if they are, set the corresponding bit in key_status
+        if (scancode == 0xAA || scancode == 0xB6) {
+            // left or right shift released
+            key_status &= ~0x01;
+        } else if (scancode == 0x9D) {
+            // ctrl released
+            key_status &= ~0x04;
+        } else if (scancode == 0xB8) {
+            // alt released
+            key_status &= ~0x08;
+        } else if (scancode == 0xBA) {
+            // caps lock released
+            key_status &= ~0x10;
+        } else if (scancode == 0xC5) {
+            // num lock released
+            key_status &= ~0x20;
+        } else if (scancode == 0xC6) {
+            // scroll lock released
+            key_status &= ~0x40;
+        }
+    } else {
+        // key pressed
+        // handle key down event
+        // check if shift, ctrl, alt, caps lock, num lock, scroll lock are pressed
+        // if they are, set the corresponding bit in key_status
+        // if not, print the character corresponding to the key pressed
+        // if the key is a special key, handle it accordingly
+        if (scancode == 0x2A || scancode == 0x36) {
+            // left or right shift pressed
+            key_status |= 0x01;
+        } else if (scancode == 0x1D) {
+            // ctrl pressed
+            key_status |= 0x04;
+        } else if (scancode == 0x38) {
+            // alt pressed
+            key_status |= 0x08;
+        } else if (scancode == 0x3A) {
+            // caps lock pressed
+            key_status |= 0x10;
+        } else if (scancode == 0x45) {
+            // num lock pressed
+            key_status |= 0x20;
+        } else if (scancode == 0x46) {
+            // scroll lock pressed
+            key_status |= 0x40;
+        } else {
+            // print the character corresponding to the key pressed
+            if (key_status & 0x10) {
+                // caps lock is on
+                // if scancode is a letter, print the upper case letter
+                // else print the normal character corresponding to the scancode
+                if ((scancode >= 0x10 && scancode <= 0x19) || (scancode >= 0x1E && scancode <= 0x26) || (scancode >= 0x2C && scancode <= 0x32)) {
+                    // print upper case letter
+                    putc(keymap_upper[scancode]);
+                } else {
+                    // print normal character
+                    putc(keymap[scancode]);
+                }
+            } else {
+                // caps lock is off
+                // if shift is on
+                if (key_status & 0x01) {
+                    putc(keymap_upper[scancode]);
+                } else {
+                    putc(keymap[scancode]);
+                }
+            }
+        }
+
     }
+
     send_eoi(KEYBOARD_IRQ);                 /* send EOI */
 }
