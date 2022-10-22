df5797e32b333b1fa62065d1aec392b0534e0df7
Vasilis <vasilis2@illinois.edu>
2022-10-22 06:22:03 -0500

    TODO: read_data, file_read, and dir_read

--- student-distrib/filesystem.h
+++ student-distrib/filesystem.h
@@ -11,8 +11,23 @@ struct dentry
 {
 	char name[32];	//if all 32 chars are filled no '\0'
 	uint32_t ft;
-	uint32_t idx;
+	uint32_t ind;
 	uint32_t reserve	: 24;
+} __attribute__((packed));
+
+struct inode
+{
+	uint32_t len;
+	uint32_t data[1023];
+} __attribute__((packed));
+
+struct bootblock
+{
+	uint32_t nent;
+	uint32_t nnod;
+	uint32_t nblck;
+	uint32_t res[13];		//first 64 bytes
+	struct dentry dirs[63];	//63 groups of 64 bytes
 } __attribute__((packed));
 
 int32_t read_dentry_by_name(const uint8_t* fname, struct dentry* dent);
@@ -21,6 +36,20 @@ int32_t read_dentry_by_index(uint32_t i, struct dentry* dent);
 
 int32_t read_data(uint32_t inode, uint32_t off, uint8_t* buf, uint32_t len);
 
-void filesystem_init(uint32_t* add);
+int32_t dir_open(const uint8_t* fn);
+
+int32_t dir_read(int32_t fd, void* buf, int32_t n);
+
+int32_t dir_write(int32_t fd, const void* buf, int32_t n);
+
+int32_t dir_close(int32_t fd);
+
+int32_t file_open(const uint8_t* fn);
+
+int32_t file_read(int32_t fd, void* buf, int32_t n);
+
+int32_t file_write(int32_t fd, const void* buf, int32_t n);
+
+int32_t file_close(int32_t fd);
 
 #endif
