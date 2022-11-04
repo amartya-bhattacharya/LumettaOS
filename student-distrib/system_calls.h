26b35fb3197d044334f8d1e4e56227d4d61276a0
Sherlock <nns5@illinois.edu>
2022-11-03 23:51:20 -0500

    commiting not working system calls

---
+++ student-distrib/system_calls.h
@@ -0,0 +1,5 @@
+int32_t open (const uint8 t* filename);
+int32_t write (int32_t fd, const void* buf, int32_t nbytes);
+int32_t read (int32_t fd, void* buf, int32_t nbytes);
+int32_t close (int32_t fd);
+
