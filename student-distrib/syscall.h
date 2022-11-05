b7e23c6dd042e0d831a001fb56ed470f42b8544a
Bhattacharya <amartya5@illinois.edu>
2022-11-05 02:05:53 -0500

    added user level program handler, will page fault until paging is set up

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -7,8 +7,22 @@
 
 #include "types.h"
 
+/* Page directory and page table constants */
+#define PAGE_DIR_SIZE 1024
+#define _132MB 0x08400000
+#define _128MB 0x08000000
+#define PROC_OFFSET 0x00480000
+#define _8MB 0x00800000
+#define _4MB 0x00400000
+#define _8KB 0x00002000
+#define _4KB 0x00001000
+#define MASK_8KB 0xFFFFE000
+#define MAX_PROCESSES 6
+#define KERNEL_STACK_BOTTOM (PAGE_DIR_SIZE - 1) * _4KB
+
+/* Externally-visible functions */
 int32_t system_execute(const uint8_t * command);
-int32_t open (const uint8 t* filename);
+int32_t open (const uint8_t* filename);
 int32_t write (int32_t fd, const void* buf, int32_t nbytes);
 int32_t read (int32_t fd, void* buf, int32_t nbytes);
 int32_t close (int32_t fd);
