671f911f875a17b76c23f7cc12ed383afc9d8cfe
Vasilis <vasilis2@illinois.edu>
2022-12-03 02:44:26 -0600

    commit for amartya

--- student-distrib/scheduling.c
+++ student-distrib/scheduling.c
@@ -1,13 +1,13 @@
-// /* scheduling.c - Functions used to interact with the scheduler
-//  * vim:ts=4 sw=4 noexpandtab
-//  */
+/* scheduling.c - Functions used to interact with the scheduler
+ * vim:ts=4 sw=4 noexpandtab
+ */
 
-// #include "scheduling.h"
+#include "scheduling.h"
 
-// /* Local variables */
-// volatile int32_t current_terminal;
-// volatile int32_t current_process;
-// volatile int32_t current_process_pid[3] = {0};
+/* Local variables */
+volatile int32_t current_terminal;
+volatile int32_t current_process;
+volatile int32_t current_process_pid[3] = {0};
 
 
 // void init_scheduling(void) {
