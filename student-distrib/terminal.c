357e9d7261a582cbbf7c7fb7187812a420f723b6
Amartya Bhattacharya <amartyacharya@gmail.com>
2022-12-02 21:09:25 -0600

    fixed rtc virtualization

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -7,12 +7,25 @@
 #include "keyboard.h"
 
 /* Local variables */
+// must have a separate input buffer for each terminal
+// must save save the current text screen and cursor position as the terminal state
+// switching terminals is equivalent to switching the terminal states
 
 // returns only when the enter key is pressed
 // always add a newline character to the end of the buffer before returning
 // 128 character limit includes the newline character
 // handle buffer overflow (more than 127 characters)
 // handle situations where the buffer is not full but the enter key is pressed
+
+/* terminal_read
+ * DESCRIPTION: Reads from the keyboard buffer
+ * INPUTS: fd - file descriptor
+ *         buf - buffer to read into
+ *         nbytes - number of bytes to read
+ * OUTPUTS: NONE
+ * RETURN VALUE: number of bytes read
+ * SIDE EFFECTS: Reads from the keyboard buffer
+ */
 int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes) {
     // read from keyboard buffer to buf until a newline '\n' or as much as fits in the buffer
     // if the buffer is full, return -1
@@ -33,24 +46,6 @@ int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes) {
         if ((char) keyboard_buffer[i] == '\n')
         break;
     }
-        
-        // for (i = 0; i < nbytes; i++) {
-        //     if ((char) keyboard_buffer[i] == '\n') {
-        //         puts("entered\n");
-        //         break;
-        //     }
-        //     *(((char*)buf) + i) = keyboard_buffer[i];         // copy from keyboard buffer to buf
-        // }
-        // c = keyboard_buffer[i];
-        // while(enterpress
-        // temp_buffer[i] = keyboard_buffer[i];
-        // if ((char) keyboard_buffer[i] == '\n') {
-        //     // puts("entered");
-        //     break;
-        // }
-        // i++;
-        // sti();
-
 
     //clear buffer
     //putc(i + '0');
